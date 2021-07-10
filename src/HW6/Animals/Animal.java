0237c4159a25cdc0eae2153fab5a5e5dde7da886
Aleksey Markin <traldar@mail.ru>
2021-07-10 22:43:06 +0300

    HW

--- src/HW6/Animals/Animal.java
+++ src/HW6/Animals/Animal.java
@@ -1,4 +1,17 @@
 package HW6.Animals;
 
-public class Animal {
+abstract class Animal {
+    String name;
+    String color;
+    int age;
+
+    public Animal(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+
+    abstract void run(int run);
+
+    abstract void swim(int swim);
 }
--- src/HW6/Main.java
+++ src/HW6/Main.java
@@ -1,7 +1,18 @@
 package HW6;
 
+import HW6.Animals.Cat;
+import HW6.Animals.Dog;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        Cat cat = new Cat("Барсик", "Белый", 5);
+        Dog dog = new Dog("Bobik", "black", 3);
+
+        dog.run(300);
+        cat.run(300);
+
+        dog.swim(5);
+        cat.swim(5);
     }
 }
